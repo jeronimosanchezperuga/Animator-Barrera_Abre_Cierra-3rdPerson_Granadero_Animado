f3ccad7f4876a35e226ce421d554a1671dcd88b2
jsanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2022-11-03 15:11:18 -0300

    Players get destroyed and return to lobby

---
+++ Assets/IrALobby.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class IrALobby : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        Debug.Log("Clickeado");
+        PhotonNetwork.Destroy(gameObject);
+        PhotonNetwork.LeaveRoom();
+        PhotonNetwork.LoadLevel("Loading");
+    }
+
+    public override void OnLeftRoom()
+    {
+        Debug.Log("Salio de la room");
+        PhotonNetwork.LoadLevel("Loading");
+    }
+}
